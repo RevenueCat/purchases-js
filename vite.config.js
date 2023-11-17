f6a180875072b96d1b85c137d7254c4b87415f6f
Alfonso Embid-Desmet <alfperata@gmail.com>
2023-11-16 16:21:26 -0800

    only build es modules

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: "Purchases",
-      formats: ["umd", "iife", "es"]
+      formats: ["es"]
     },
   },
   plugins: [dts()]
