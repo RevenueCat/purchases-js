1b273d12dc3552a9577b2118823bd91ea73f31d1
Nicola Sacco <nicola@revenuecat.com>
2023-11-20 16:07:56 +0100

    Added the waitForEntitlement method

--- src/main.ts
+++ src/main.ts
@@ -81,6 +81,35 @@ export class Purchases {
     return entitlements.includes(entitlementIdentifier);
   }
 
+  public async waitForEntitlement(
+    appUserId: string,
+    entitlementIdentifier: string,
+    maxAttempts: number = 1,
+  ): Promise<boolean> {
+    const waitMSBetweenAttempts = 1000;
+    return new Promise<boolean>((resolve, reject) => {
+      const checkForEntitlement = (checkCount = 1) =>
+        this.isEntitledTo(appUserId, entitlementIdentifier)
+          .then((hasEntitlement) => {
+            if (checkCount >= maxAttempts) {
+              return resolve(false);
+            }
+
+            if (hasEntitlement) {
+              return resolve(true);
+            } else {
+              setTimeout(
+                () => checkForEntitlement(checkCount + 1),
+                waitMSBetweenAttempts,
+              );
+            }
+          })
+          .catch(reject);
+
+      checkForEntitlement();
+    });
+  }
+
   public async subscribe(
     appUserId: string,
     productId: string,
