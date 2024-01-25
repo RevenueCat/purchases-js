060e43c01e79d47c9616348577e913b848f4eb3b
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-01-25 12:41:07 +0100

    Add handling for networking errors

--- src/networking/http-client.ts
+++ src/networking/http-client.ts
@@ -1,6 +1,12 @@
 import { SupportedEndpoint } from "./endpoints";
-import { ServerError } from "../entities/errors";
+import {
+  BackendErrorCode,
+  ErrorCode,
+  ErrorCodeUtils,
+  PurchasesError,
+} from "../entities/errors";
 import { VERSION } from "../helpers/constants";
+import { StatusCodes } from "http-status-codes";
 
 export async function performRequest<RequestBody, ResponseType>(
   endpoint: SupportedEndpoint,
@@ -14,12 +20,46 @@ export async function performRequest<RequestBody, ResponseType>(
     body: getBody(body),
   });
 
-  // TODO: Improve error handling
-  if (response.status >= 400) {
-    throw new ServerError(response.status, await response.text());
-  }
+  await handleErrors(response, endpoint);
 
   return (await response.json()) as ResponseType; // TODO: Validate response is correct.
+}
+
+async function handleErrors(response: Response, endpoint: SupportedEndpoint) {
+  const statusCode = response.status;
+  if (statusCode >= StatusCodes.INTERNAL_SERVER_ERROR) {
+    throw new PurchasesError(
+      ErrorCode.UnknownBackendError,
+      `Server responded with status code: ${statusCode} in request: ${endpoint.name}.`,
+    );
+  } else if (statusCode >= StatusCodes.BAD_REQUEST) {
+    const errorBody = await response.json();
+    const backendErrorCodeNumber: number | null = errorBody.code;
+    const backendErrorMessage: string | null = errorBody.message;
+    if (backendErrorCodeNumber != null) {
+      const backendErrorCode: BackendErrorCode | null =
+        ErrorCodeUtils.convertCodeToBackendErrorCode(backendErrorCodeNumber);
+      if (backendErrorCode == null) {
+        throw new PurchasesError(
+          ErrorCode.UnknownBackendError,
+          `Unknown backend error code. Request: ${endpoint.name}. Status code: ${statusCode}. Body: ${await response.text()}.`,
+        );
+      } else {
+        const errorCode =
+          ErrorCodeUtils.getErrorCodeForBackendErrorCode(backendErrorCode);
+        throw new PurchasesError(
+          errorCode,
+          ErrorCodeUtils.getPublicMessage(errorCode),
+          backendErrorMessage,
+        );
+      }
+    } else {
+      throw new PurchasesError(
+        ErrorCode.UnknownBackendError,
+        `Unknown backend error. Request: ${endpoint.name}. Status code: ${statusCode}. Body: ${await response.text()}.`,
+      );
+    }
+  }
 }
 
 function getBody<RequestBody>(body?: RequestBody): string | null {
