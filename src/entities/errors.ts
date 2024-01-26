e6c016c026f980b5009ad6b994a901507552447d
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-01-26 13:11:06 +0100

    Remove unused error classes

--- src/entities/errors.ts
+++ src/entities/errors.ts
@@ -184,26 +184,3 @@ export class PurchasesError extends Error {
     return `PurchasesError(code: ${ErrorCode[this.errorCode]}, message: ${this.message})`;
   };
 }
-
-export class ServerError extends Error {
-  constructor(
-    public readonly statusCode: number,
-    message?: string | undefined,
-  ) {
-    super(message);
-  }
-}
-
-export class UnknownServerError extends ServerError {
-  constructor() {
-    super(StatusCodes.INTERNAL_SERVER_ERROR, "An unknown error occurred.");
-  }
-}
-
-export class InvalidInputDataError extends ServerError {}
-
-export class AlreadySubscribedError extends ServerError {}
-
-export class PaymentGatewayError extends ServerError {}
-
-export class ConcurrentSubscriberAttributeUpdateError extends ServerError {}
