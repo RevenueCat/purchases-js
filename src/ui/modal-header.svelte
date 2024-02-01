6c4d56730837b5629c41e1e32c39e76ed3d17229
Franco Correa <franco@francocorrea.com>
2024-02-01 10:23:50 -0300

    Do not show icon if not available

--- src/ui/modal-header.svelte
+++ src/ui/modal-header.svelte
@@ -13,7 +13,9 @@
   <div class="rcb-header-layout">
     <div class="rcb-header-layout__business-info">
       {#if brandingInfo !== null}
-        <AppIcon src={buildAssetURL(brandingInfo.app_icon_webp)} />
+        {#if brandingInfo.app_icon_webp !== null}
+          <AppIcon src={buildAssetURL(brandingInfo.app_icon_webp)} />
+        {/if}
         <span class="app-title">{brandingInfo.seller_company_name}</span>
       {:else}
         <AppIcon />
