8d20c5da18de9450c71a3f05562daca9ef6c47ca
Franco Correa <franco@francocorrea.com>
2024-02-02 09:26:21 -0300

    Fixes

--- src/ui/shell.svelte
+++ src/ui/shell.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import ModalHeader from "./modal-header.svelte";
   import Modal from "./modal.svelte";
-  import { BrandingInfoResponse } from "../entities/types";
+  import { BrandingInfoResponse } from "../networking/responses/branding-response";
 
   export let title = "";
   export let dark = false;
   export let showHeader = false;
-  export let brandingInfo: BrandingInfoResponse | null;
+  export let brandingInfo: BrandingInfoResponse | null = null;
 </script>
 
 <Modal {dark}>
