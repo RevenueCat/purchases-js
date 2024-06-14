cbb787881f8927df19ad132d00704df5fcc4d3eb
Jens-Fabian Goetzmann <jens@revenuecat.com>
2024-06-14 16:22:04 +0200

    Make demo app independent of entitlement ID

--- examples/rcbilling-demo/src/components/WithoutEntitlement.tsx
+++ examples/rcbilling-demo/src/components/WithoutEntitlement.tsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from "react";
-import IChildrenProps from "./IChildrenProps";
+import React, { PropsWithChildren, useEffect, useState } from "react";
 import { usePurchasesLoaderData } from "../util/PurchasesLoader";
 import { useNavigate } from "react-router-dom";
 
-interface IWithoutEntitlementProps extends IChildrenProps {
-  entitlementId: string;
-}
-
-const WithoutEntitlement: React.FC<IWithoutEntitlementProps> = ({
-  entitlementId,
-  children,
-}) => {
+const WithoutEntitlement: React.FC<PropsWithChildren> = ({ children }) => {
   const { customerInfo, purchases } = usePurchasesLoaderData();
   const [isLoading, setLoading] = useState<boolean>(true);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (customerInfo.entitlements.active[entitlementId]) {
+    if (Object.keys(customerInfo.entitlements.active).length > 0) {
       navigate(`/success/${purchases.getAppUserId()}`);
     } else {
       setLoading(false);
     }
-  }, [customerInfo, purchases, entitlementId, navigate]);
+  }, [customerInfo, purchases, navigate]);
 
   return customerInfo && !isLoading ? <>{children}</> : null;
 };
