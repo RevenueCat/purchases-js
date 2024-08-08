9cee7d2509b4faab04619cba306c88428534028c
Guido Torres <guido@revenuecat.com>
2024-08-08 17:12:13 -0300

    remove puppeteer and add playwright

---
+++ examples/rcbilling-demo/playwright-global-setup.ts
@@ -0,0 +1,6 @@
+// We can use this function to define variables that will only be available
+// within the test method
+// https://playwright.dev/docs/test-global-setup-teardown
+async function globalSetup() {}
+
+export default globalSetup;
