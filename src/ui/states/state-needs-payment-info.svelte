24a151f52b163cb2229f13780d0c82ae070c94a3
Jens-Fabian Goetzmann <jens@revenuecat.com>
2024-08-26 09:48:30 +0200

    Make main CTA not say "Pay" if the customer is starting a trial

--- src/ui/states/state-needs-payment-info.svelte
+++ src/ui/states/state-needs-payment-info.svelte
@@ -17,12 +17,15 @@
   import IconLock from "../assets/icon-lock.svelte";
   import { Colors } from "../../assets/colors";
   import ProcessingAnimation from "../processing-animation.svelte";
+  import { Product, PurchaseOption } from "../../main";
 
   export let onClose: any;
   export let onContinue: any;
   export let onError: any;
   export let paymentInfoCollectionMetadata: SubscribeResponse;
   export let processing = false;
+  export let productDetails: Product;
+  export let purchaseOptionToUse: PurchaseOption;
 
   const clientSecret = paymentInfoCollectionMetadata.data.client_secret;
 
@@ -125,10 +128,12 @@
             <Button disabled={processing}>
               {#if processing}
                 <ProcessingAnimation />
+              {:else if productDetails.subscriptionOptions[purchaseOptionToUse.id].trial !== null}
+                Start Trial
               {:else}
                 Pay
-              {/if}</Button
-            >
+              {/if}
+            </Button>
             <Button disabled={processing} intent="secondary" on:click={onClose}
               >Close</Button
             >
