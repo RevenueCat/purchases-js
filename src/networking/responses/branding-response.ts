ab95d313e0981310f15a7a43fb3ab5a36ce838fa
Guido Torres <guido@revenuecat.com>
2024-09-03 15:50:51 -0300

    add appearance type to branding info response

--- src/networking/responses/branding-response.ts
+++ src/networking/responses/branding-response.ts
@@ -1,6 +1,7 @@
 export type BrandingInfoResponse = {
   app_icon: string | null;
   app_icon_webp: string | null;
+  appearance: Record<string, string | boolean>;
   id: string;
   seller_company_name: string | null;
   seller_company_support_email?: string | null;
