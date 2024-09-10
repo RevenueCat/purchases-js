ab3f04b7ac6c6c4d127a944a30786610387c828d
Guido Torres <guido@revenuecat.com>
2024-09-10 17:28:15 -0300

    apply color customization to icon components

--- src/ui/assets/icon-lock.svelte
+++ src/ui/assets/icon-lock.svelte
@@ -1,5 +1,21 @@
 <script>
-  import Icon from "../../assets/lock.svg";
+  import { getStyleVariable } from "../../helpers/process-style-overrides";
+  import { appearanceConfigStore } from "../../store/store";
+  import Icon from "../../assets/lock.svg?raw"; // Load SVG as a raw string
+
+  let svgUrl;
+  const accentColor = $appearanceConfigStore["color_accent"];
+  let svgContent = Icon;
+
+  if (accentColor) {
+    svgContent = Icon.replace(
+      /fill="#[0-9A-Fa-f]{6}"/,
+      `fill="${accentColor}"`,
+    );
+    svgUrl = `data:image/svg+xml,${encodeURIComponent(svgContent)}`;
+  } else {
+    svgUrl = `data:image/svg+xml,${encodeURIComponent(Icon)}`;
+  }
 </script>
 
-<img src={Icon} alt="icon" class="rcb-ui-asset-icon" />
+<img src={svgUrl} alt="icon" class="rcb-ui-asset-icon" />
--- src/ui/assets/icon-success.svelte
+++ src/ui/assets/icon-success.svelte
@@ -1,8 +1,25 @@
 <script>
-  import Icon from "../../assets/success.svg";
+  import Icon from "../../assets/success.svg?raw"; // Load SVG as a raw string
+
+  import { getStyleVariable } from "../../helpers/process-style-overrides";
+  import { appearanceConfigStore } from "../../store/store";
+
+  let svgUrl;
+  const accentColor = $appearanceConfigStore["color_accent"];
+  let svgContent = Icon;
+
+  if (accentColor) {
+    svgContent = Icon.replace(
+      /fill="#[0-9A-Fa-f]{6}"/,
+      `fill="${accentColor}"`,
+    );
+    svgUrl = `data:image/svg+xml,${encodeURIComponent(svgContent)}`;
+  } else {
+    svgUrl = `data:image/svg+xml,${encodeURIComponent(Icon)}`;
+  }
 </script>
 
-<img src={Icon} alt="icon" class="rcb-ui-asset-icon" />
+<img src={svgUrl} alt="icon" class="rcb-ui-asset-icon" />
 
 <style>
   img {
