ed92f9c5d53600a08a773d8bdfdaa5aa866732c6
Guido Torres <guidotorres@192.168.1.13>
2024-10-04 13:18:48 -0300

    define types for appearance object

--- src/networking/responses/branding-response.ts
+++ src/networking/responses/branding-response.ts
@@ -1,7 +1,19 @@
+interface BrandingAppearance {
+  color_buttons_primary: string;
+  color_accent: string;
+  color_error: string;
+  color_product_info_bg: string;
+  color_form_bg: string;
+  color_page_bg: string;
+  font: string;
+  shapes: "default" | "rectangle" | "rounded" | "pill";
+  show_product_description: boolean;
+}
+
 export type BrandingInfoResponse = {
   app_icon: string | null;
   app_icon_webp: string | null;
-  appearance: Record<string, string | boolean>;
+  appearance: BrandingAppearance;
   id: string;
   seller_company_name: string | null;
   seller_company_support_email?: string | null;
