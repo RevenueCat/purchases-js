1ebfb5765607b1c642e5a9afaa1ce234e45fe5e9
Nicola Sacco <nicola@revenuecat.com>
2024-10-10 15:26:33 +0200

    Updated storybook Fixed the main.ts setup Started working on a story for the components affected by customisation

---
+++ src/stories/state-present-offer.stories.svelte
@@ -0,0 +1,62 @@
+<script>
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import StatePresentOffer from "../ui/states/state-present-offer.svelte";
+  import Shell from "../ui/shell.svelte";
+  import { mapStyleOverridesToStyleVariables } from "../helpers/process-style-overrides.ts";
+
+
+  let subscriptionOption={
+    id:"option_id_1",
+    priceId:'price_1',
+    base: {
+      periodDuration: "P1M",
+      price: {
+        id:'price_1',
+        amount: 999,
+        amountMicros:999,
+        currency: "USD",
+        formattedPrice: "9.99$",
+      },
+      cycleCount: 0,
+    },
+  }
+
+  let product = {
+    identifier: "some_product_123",
+    displayName: "Some Product 123",
+    title:"Some Product 123",
+    productType: "subscription",
+    currentPrice: {
+      amount: 999,
+      amountMicros:999,
+      currency: "USD",
+      formattedPrice: "9.99$",
+    },
+    normalPeriodDuration: "P1M",
+    presentedOfferingIdentifier: "some_offering_identifier",
+    defaultSubscriptionOption: subscriptionOption,
+    subscriptionOptions: {
+      option_id_1: subscriptionOption
+    },
+  };
+</script>
+
+
+<Meta title="StoryContext" component={StatePresentOffer} />
+
+
+
+<Template let:args>
+    <div class="rcb-ui-container" style={mapStyleOverridesToStyleVariables(args?.brandingAppearance)}>
+      <div class="rcb-ui-aside">
+      <Shell dark>
+        <StatePresentOffer {...args} />
+      </Shell>
+      </div>
+    </div>
+</Template>
+
+
+<Story name='Primary' args={{ productDetails:product, purchaseOption:subscriptionOption, brandingAppearance:{
+
+} }} />
