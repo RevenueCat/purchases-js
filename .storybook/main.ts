13a41aecc7600f36ac7a537daf41875bf38f2a3b
Nicola Sacco <nicola@revenuecat.com>
2024-10-10 16:25:56 +0200

    Formatted with prettier

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-svelte-csf"
+    "@storybook/addon-svelte-csf",
   ],
   framework: "@storybook/svelte-vite",
   docs: {
