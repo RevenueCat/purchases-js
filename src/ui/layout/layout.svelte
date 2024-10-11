d74d85c79bd4ee7e91dbd70b690662c255cd8190
Nicola Sacco <nicola@revenuecat.com>
2024-10-11 10:35:55 +0200

    ECO-1336: extracted layout components into dedicated svelte files (#207)
    ## Motivation / Description
    The style for the layout components was defined at rcb-ui level.
    
    This made it harder to write stories reusing the style (apart from copy
    pasting it).
    
    This PR refactors the existing code to create some layout components
    that match the ones used in the rcb-ui entrypoint component. It also
    moves the style in the specific components.
    
    ## Changes introduced
    * Created the aside-block, main-block, layout and container components
    * Used those components in rcb-ui and cleaned up the style from there
    * Used those components in the state-present-offer.stories.svelte so
    that now I can see the result of the customisation changes (there was no
    style before)

---
+++ src/ui/layout/layout.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let style = "";
+</script>
+
+
+<div class="rcb-ui-layout" style={style}>
+  <slot />
+</div>
+
+
+<style>
+    .rcb-ui-layout {
+        width: 100vw;
+        margin-right: auto;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        position: relative;
+        padding: 0px 80px;
+        box-sizing: border-box;
+    }
+
+    @media screen and (max-width: 960px) {
+        .rcb-ui-layout {
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+            min-width: 100%;
+        }
+    }
+
+    @media screen and (max-width: 960px) and (max-height: 960px) {
+        .rcb-ui-layout {
+            overflow-y: scroll;
+            justify-content: flex-start;
+            padding: 16px 0;
+        }
+    }
+</style>
---
+++ src/ui/layout/main-block.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let style = "";
+</script>
+
+<div class="rcb-ui-main" style={style}>
+  <slot />
+</div>
+
+<style>
+    .rcb-ui-main {
+        flex: 480px 1 0;
+        max-width: 640px;
+    }
+
+    @media screen and (max-width: 960px) {
+
+        .rcb-ui-main {
+            flex: none;
+            max-width: none;
+            min-width: 100%;
+        }
+    }
+</style>
