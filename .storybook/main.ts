77e6b924c8d931c70fff12c60551b1c947f10c49
Nicola Sacco <nicola@revenuecat.com>
2024-10-11 09:02:44 +0200

    Fixing the storybook set up to make it easier to implement UI changes in the sdk (#206)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,11 +6,9 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-svelte-csf",
   ],
-  framework: {
-    name: "@storybook/svelte-vite",
-    options: {},
-  },
+  framework: "@storybook/svelte-vite",
   docs: {
     autodocs: "tag",
   },
