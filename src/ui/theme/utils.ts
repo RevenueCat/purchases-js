d6f17db2f0c583beb42a214db3f712819296d165
Nicola Sacco <nicola@revenuecat.com>
2024-10-28 17:31:39 +0100

    Customization: Aligned luminance with the one in the Customer Portal and the WPL (#224)

--- src/ui/theme/utils.ts
+++ src/ui/theme/utils.ts
@@ -52,7 +52,7 @@ const isLightColor = ({ r, g, b }: RGB) => {
     0.0722 * gammaCorrect(b);
 
   // Return whether the background is light
-  return luminance > 0.179;
+  return luminance > 0.2;
 };
 
 const rgbToTextColors = (rgb: RGB) => {
