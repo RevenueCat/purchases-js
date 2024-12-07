a99fc543aedaf99f0d9b97cbaf34019113041664
vicfergar <victor@revenuecat.com>
2024-12-07 10:59:04 +0100

    chore: Rename utils component to `StateNeedsPaymentInfoWithPurchaseResponse`

--- src/stories/utils/state-needs-payment-info-with-purchase-response.svelte
+++ src/stories/utils/state-needs-payment-info-with-purchase-response.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { type ComponentProps, onMount } from "svelte";
   import { type PurchaseResponse } from "../../networking/responses/purchase-response";
-  import { type BrandingInfoResponse } from "../../networking/responses/branding-response";
-  import { product, subscriptionOption } from "../fixtures";
   import { buildPurchaseResponse } from "./purchase-response-builder";
   import StateNeedsPaymentInfo from "../../ui/states/state-needs-payment-info.svelte";
   import StateLoading from "../../ui/states/state-loading.svelte";
-  import Main from "../../ui/layout/main-block.svelte";
-  export let brandingInfo: BrandingInfoResponse | null;
+
+  export let args: ComponentProps<StateNeedsPaymentInfo>;
 
   let paymentMetadata: PurchaseResponse | null = null;
-  let defaultArgs = {
-    productDetails: product,
-    purchaseOptionToUse: subscriptionOption,
+  let overriddenArgs = {
     onClose: () => {},
     onContinue: async () => {
       alert("Payment info submitted successfully!\n The form will be reset");
@@ -26,14 +22,12 @@
   });
 </script>
 
-<Main brandingAppearance={brandingInfo?.appearance}>
-  {#if paymentMetadata}
-    <StateNeedsPaymentInfo
-      {...defaultArgs}
-      {brandingInfo}
-      paymentInfoCollectionMetadata={paymentMetadata}
-    />
-  {:else}
-    <StateLoading />
-  {/if}
-</Main>
+{#if paymentMetadata}
+  <StateNeedsPaymentInfo
+    {...args}
+    {...overriddenArgs}
+    paymentInfoCollectionMetadata={paymentMetadata}
+  />
+{:else}
+  <StateLoading />
+{/if}
