b0923938d7d98f87b55de1964acf422923d81c67
Pol Miro <polmiro@gmail.com>
2025-01-31 23:19:22 +0100

    Separate event helpers

---
+++ src/behavioural-events/event.ts
@@ -0,0 +1,56 @@
+import { camelToUnderscore } from "../helpers/camel-to-underscore";
+import { v4 as uuidv4 } from "uuid";
+
+type EventPropertyValue =
+  | string
+  | number
+  | boolean
+  | null
+  | EventPropertyArray
+  | EventProperties;
+
+export interface EventProperties {
+  [key: string]: EventPropertyValue;
+}
+
+type EventPropertyArray = Array<EventPropertyValue>;
+
+export type EventData = {
+  eventName: string;
+  traceId: string;
+  checkoutSessionId: string | null;
+  appUserId: string;
+  userIsAnonymous: boolean;
+  properties: EventProperties;
+};
+
+export class Event {
+  EVENT_TYPE = "web_billing";
+
+  public readonly id: string;
+  public readonly timestampMs: number;
+  public readonly data: EventData;
+
+  public constructor(data: EventData) {
+    this.id = uuidv4();
+    this.timestampMs = Date.now();
+    this.data = data;
+  }
+
+  public toJSON(): Record<
+    string,
+    string | number | boolean | null | EventProperties
+  > {
+    return {
+      id: this.id,
+      timestamp_ms: this.timestampMs,
+      type: this.EVENT_TYPE,
+      event_name: this.data.eventName,
+      trace_id: this.data.traceId,
+      checkout_session_id: this.data.checkoutSessionId,
+      app_user_id: this.data.appUserId,
+      user_is_anonymous: this.data.userIsAnonymous,
+      properties: camelToUnderscore(this.data.properties) as EventProperties,
+    };
+  }
+}
