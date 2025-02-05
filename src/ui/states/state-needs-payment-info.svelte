0ed343f28b204f181fe4dfeb7d0675f65ca08ae0
vicfergar <victor@revenuecat.com>
2025-02-05 12:55:22 +0100

    chore: Remove `svelte-stripe` dependency

--- src/ui/states/state-needs-payment-info.svelte
+++ src/ui/states/state-needs-payment-info.svelte
@@ -2,15 +2,14 @@
   import { getContext, onMount } from "svelte";
 
   import Button from "../button.svelte";
-  import { Elements, PaymentElement } from "svelte-stripe";
   import type {
+    Appearance,
     Stripe,
     StripeElementLocale,
     StripeElements,
     StripeError,
   } from "@stripe/stripe-js";
   import { loadStripe } from "@stripe/stripe-js";
-  import StateLoading from "./state-loading.svelte";
   import { type PurchaseResponse } from "../../networking/responses/purchase-response";
   import ProcessingAnimation from "../processing-animation.svelte";
   import type { Product, PurchaseOption } from "../../entities/offerings";
@@ -45,7 +44,7 @@
   let textStyles = new Theme().textStyles;
   let spacing = new Theme().spacing;
 
-  let stripeVariables: undefined | Elements["variables"];
+  let stripeVariables: undefined | Appearance["variables"];
   let viewport: "mobile" | "desktop" = "mobile";
 
   // Maybe extract this to a
@@ -71,6 +70,46 @@
       updateStripeVariables();
     }, 150);
   }
+
+
+  const theme = new Theme(brandingInfo?.appearance);
+
+  let customShape = theme.shape;
+  let customColors = theme.formColors;
+
+  const translator: Translator =
+    getContext(translatorContextKey) || Translator.fallback();
+  const stripeElementLocale = (translator.locale ||
+    translator.fallbackLocale) as StripeElementLocale;
+
+  /**
+   * This function converts some particular locales to the ones that stripe supports.
+   * Finally falls back to 'auto' if the initialLocale is not supported by stripe.
+   * @param initialLocale
+   */
+  const getLocaleToUse = (initialLocale: string) => {
+    // These locale that we support are not supported by stripe.
+    // if any of these is passed we fallback to 'auto' so that
+    // stripe will pick up the locale from the browser.
+    const stripeUnsupportedLocale = ["ca", "hi", "uk"];
+
+    if (stripeUnsupportedLocale.includes(initialLocale)) {
+      return "auto" as StripeElementLocale;
+    }
+
+    const mappedLocale: Record<string, string> = {
+      zh_Hans: "zh",
+      zh_Hant: "zh",
+    };
+
+    if (Object.keys(mappedLocale).includes(initialLocale)) {
+      return mappedLocale[initialLocale] as StripeElementLocale;
+    }
+
+    return initialLocale as StripeElementLocale;
+  };
+
+  const localeToUse = getLocaleToUse(stripeElementLocale);
 
   onMount(() => {
     updateStripeVariables();
@@ -99,6 +138,105 @@
 
     stripe = await loadStripe(stripePk, {
       stripeAccount: stripeAcctId,
+    });
+
+    if (!stripe) {
+      throw new Error("Stripe client not found");
+    }
+
+    elements = stripe.elements(
+      {
+        loader: "always",
+        locale: localeToUse,
+        mode: "payment",
+        paymentMethodTypes: ["card"],
+        setup_future_usage: "off_session",
+        amount: productDetails.currentPrice.amountMicros / 10000,
+        currency: productDetails.currentPrice.currency.toLowerCase(),
+        appearance: {
+          theme: "stripe",
+          labels: "floating",
+          variables: {
+            borderRadius: customShape["input-border-radius"],
+            fontLineHeight: "10px",
+            focusBoxShadow: "none",
+            colorDanger: customColors["error"],
+            colorTextPlaceholder: customColors["grey-text-light"],
+            colorText: customColors["grey-text-dark"],
+            colorTextSecondary: customColors["grey-text-light"],
+            ...stripeVariables,
+          },
+          rules: {
+            ".Input": {
+              boxShadow: "none",
+              paddingTop: "6px",
+              paddingBottom: "6px",
+              border: `1px solid ${customColors["grey-ui-dark"]}`,
+              backgroundColor: customColors["input-background"],
+              color: customColors["grey-text-dark"],
+            },
+            ".Input:focus": {
+              border: `1px solid ${customColors["focus"]}`,
+              outline: "none",
+            },
+            ".Label": {
+              fontWeight: textStyles.body1[viewport].fontWeight,
+              lineHeight: "22px",
+              color: customColors["grey-text-dark"],
+            },
+            ".Label--floating": {
+              fontSize: "10px !important",
+            },
+            ".Input--invalid": {
+              boxShadow: "none",
+            },
+            ".TermsText": {
+              fontSize: textStyles.caption[viewport].fontSize,
+              lineHeight: textStyles.caption[viewport].lineHeight,
+            },
+            ".Tab": {
+              boxShadow: "none",
+              backgroundColor: "transparent",
+              color: customColors["grey-text-light"],
+              border: `1px solid ${customColors["grey-ui-dark"]}`,
+            },
+            ".Tab:hover, .Tab:focus, .Tab--selected, .Tab--selected:hover, .Tab--selected:focus":
+              {
+                boxShadow: "none",
+                color: customColors["grey-text-dark"],
+              },
+            ".Tab:focus, .Tab--selected, .Tab--selected:hover, .Tab--selected:focus":
+              {
+                border: `1px solid ${customColors["focus"]}`,
+              },
+            ".TabIcon": {
+              fill: customColors["grey-text-light"],
+            },
+            ".TabIcon--selected": {
+              fill: customColors["grey-text-dark"],
+            },
+            ".Block": {
+              boxShadow: "none",
+              backgroundColor: "transparent",
+              border: `1px solid ${customColors["grey-ui-dark"]}`,
+            },
+          },
+        },
+      }
+    );
+
+    const paymentElement = elements.create('payment', {
+      business: brandingInfo?.app_name
+        ? { name: brandingInfo.app_name }
+        : undefined,
+      layout: {
+        type: "tabs",
+      },
+    });
+    paymentElement.mount('#payment-element');
+
+    paymentElement.on('change', (event) => {
+      isPaymentInfoComplete = event.complete;
     });
   });
 
@@ -156,190 +294,58 @@
   const handleErrorTryAgain = () => {
     modalErrorMessage = undefined;
   };
-
-  const theme = new Theme(brandingInfo?.appearance);
-
-  let customShape = theme.shape;
-  let customColors = theme.formColors;
-
-  const translator: Translator =
-    getContext(translatorContextKey) || Translator.fallback();
-  const stripeElementLocale = (translator.locale ||
-    translator.fallbackLocale) as StripeElementLocale;
-
-  type OnChangeEvent = CustomEvent<{ complete: boolean }>;
-
-  /**
-   * This function converts some particular locales to the ones that stripe supports.
-   * Finally falls back to 'auto' if the initialLocale is not supported by stripe.
-   * @param initialLocale
-   */
-  const getLocaleToUse = (initialLocale: string) => {
-    // These locale that we support are not supported by stripe.
-    // if any of these is passed we fallback to 'auto' so that
-    // stripe will pick up the locale from the browser.
-    const stripeUnsupportedLocale = ["ca", "hi", "uk"];
-
-    if (stripeUnsupportedLocale.includes(initialLocale)) {
-      return "auto" as StripeElementLocale;
-    }
-
-    const mappedLocale: Record<string, string> = {
-      zh_Hans: "zh",
-      zh_Hant: "zh",
-    };
-
-    if (Object.keys(mappedLocale).includes(initialLocale)) {
-      return mappedLocale[initialLocale] as StripeElementLocale;
-    }
-
-    return initialLocale as StripeElementLocale;
-  };
-
-  const localeToUse = getLocaleToUse(stripeElementLocale);
 </script>
 
 <div class="checkout-container">
   <!-- <TextSeparator text="Pay by card" /> -->
-  {#if stripe && clientSecret}
-    <form on:submit|preventDefault={handleContinue}>
-      <Elements
-        {stripe}
-        {clientSecret}
-        loader="always"
-        locale={localeToUse}
-        bind:elements
-        theme="stripe"
-        labels="floating"
-        variables={{
-          borderRadius: customShape["input-border-radius"],
-          fontLineHeight: "10px",
-          focusBoxShadow: "none",
-          colorDanger: customColors["error"],
-          colorTextPlaceholder: customColors["grey-text-light"],
-          colorText: customColors["grey-text-dark"],
-          colorTextSecondary: customColors["grey-text-light"],
-          ...stripeVariables,
-        }}
-        rules={{
-          ".Input": {
-            boxShadow: "none",
-            paddingTop: "6px",
-            paddingBottom: "6px",
-            border: `1px solid ${customColors["grey-ui-dark"]}`,
-            backgroundColor: customColors["input-background"],
-            color: customColors["grey-text-dark"],
-          },
-          ".Input:focus": {
-            border: `1px solid ${customColors["focus"]}`,
-            outline: "none",
-          },
-          ".Label": {
-            fontWeight: textStyles.body1[viewport].fontWeight,
-            lineHeight: "22px",
-            color: customColors["grey-text-dark"],
-          },
-          ".Label--floating": {
-            fontSize: "10px !important",
-          },
-          ".Input--invalid": {
-            boxShadow: "none",
-          },
-          ".TermsText": {
-            fontSize: textStyles.caption[viewport].fontSize,
-            lineHeight: textStyles.caption[viewport].lineHeight,
-          },
-          ".Tab": {
-            boxShadow: "none",
-            backgroundColor: "transparent",
-            color: customColors["grey-text-light"],
-            border: `1px solid ${customColors["grey-ui-dark"]}`,
-          },
-          ".Tab:hover, .Tab:focus, .Tab--selected, .Tab--selected:hover, .Tab--selected:focus":
-            {
-              boxShadow: "none",
-              color: customColors["grey-text-dark"],
-            },
-          ".Tab:focus, .Tab--selected, .Tab--selected:hover, .Tab--selected:focus":
-            {
-              border: `1px solid ${customColors["focus"]}`,
-            },
-          ".TabIcon": {
-            fill: customColors["grey-text-light"],
-          },
-          ".TabIcon--selected": {
-            fill: customColors["grey-text-dark"],
-          },
-          ".Block": {
-            boxShadow: "none",
-            backgroundColor: "transparent",
-            border: `1px solid ${customColors["grey-ui-dark"]}`,
-          },
-        }}
-      >
-        <div class="checkout-form-container" hidden={!!modalErrorMessage}>
-          <PaymentElement
-            options={{
-              business: brandingInfo?.app_name
-                ? { name: brandingInfo.app_name }
-                : undefined,
-              layout: {
-                type: "tabs",
-              },
-            }}
-            on:change={(event: OnChangeEvent) => {
-              isPaymentInfoComplete = event.detail.complete;
-            }}
-          />
+  <form on:submit|preventDefault={handleContinue}>
+    <div class="checkout-form-container" hidden={!!modalErrorMessage}>
+      <div id="payment-element"></div>
 
-          <div class="checkout-pay-container">
-            {#if !modalErrorMessage}
-              <Button
-                disabled={processing || !isPaymentInfoComplete}
-                testId="PayButton"
-              >
-                {#if processing}
-                  <ProcessingAnimation />
-                {:else if productDetails.subscriptionOptions?.[purchaseOptionToUse.id]?.trial}
-                  <Localized
-                    key={LocalizationKeys.StateNeedsPaymentInfoButtonStartTrial}
-                  />
-                {:else}
-                  <Localized
-                    key={LocalizationKeys.StateNeedsPaymentInfoButtonPay}
-                  />
-                {/if}
-              </Button>
-            {/if}
-
-            <div class="checkout-secure-container">
-              <SecureCheckoutRc />
-            </div>
-          </div>
-        </div>
-
-        {#if modalErrorMessage}
-          <MessageLayout
-            title={null}
-            type="error"
-            closeButtonTitle={translator.translate(
-              LocalizationKeys.StateErrorButtonTryAgain,
-            )}
-            onContinue={handleErrorTryAgain}
+      <div class="checkout-pay-container">
+        {#if !modalErrorMessage}
+          <Button
+            disabled={processing || !isPaymentInfoComplete}
+            testId="PayButton"
           >
-            {#snippet icon()}
-              <IconError />
-            {/snippet}
-            {#snippet message()}
-              {modalErrorMessage}
-            {/snippet}
-          </MessageLayout>
+            {#if processing}
+              <ProcessingAnimation />
+            {:else if productDetails.subscriptionOptions?.[purchaseOptionToUse.id]?.trial}
+              <Localized
+                key={LocalizationKeys.StateNeedsPaymentInfoButtonStartTrial}
+              />
+            {:else}
+              <Localized
+                key={LocalizationKeys.StateNeedsPaymentInfoButtonPay}
+              />
+            {/if}
+          </Button>
         {/if}
-      </Elements>
-    </form>
-  {:else}
-    <StateLoading />
-  {/if}
+
+        <div class="checkout-secure-container">
+          <SecureCheckoutRc />
+        </div>
+      </div>
+    </div>
+
+    {#if modalErrorMessage}
+      <MessageLayout
+        title={null}
+        type="error"
+        closeButtonTitle={translator.translate(
+          LocalizationKeys.StateErrorButtonTryAgain,
+        )}
+        onContinue={handleErrorTryAgain}
+      >
+        {#snippet icon()}
+          <IconError />
+        {/snippet}
+        {#snippet message()}
+          {modalErrorMessage}
+        {/snippet}
+      </MessageLayout>
+    {/if}
+  </form>
 </div>
 
 <style>
