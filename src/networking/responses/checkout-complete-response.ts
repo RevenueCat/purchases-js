d3a4daf747dfdad01760484ec6ab7331d34ec22f
vicfergar <victor@revenuecat.com>
2025-02-07 13:47:17 +0100

    chore: Remove unnecessary fields from `CheckoutCompleteResponse`

--- src/networking/responses/checkout-complete-response.ts
+++ src/networking/responses/checkout-complete-response.ts
@@ -2,7 +2,5 @@ export interface CheckoutCompleteResponse {
   operation_session_id: string;
   data: {
     client_secret?: string;
-    stripe_account_id?: string;
-    publishable_api_key?: string;
   };
 }
