656f84e38d77feac99d6580fa37d73f590c36d39
Alfonso Embid-Desmet <fon@revenuecat.com>
2025-02-19 14:25:05 -0800

    Fix positioning of modal of viewports of less than 320px (#330)

--- src/ui/layout/container.svelte
+++ src/ui/layout/container.svelte
@@ -51,8 +51,10 @@
 
   .rcb-ui-container.fullscreen {
     position: fixed;
-    width: 100vw;
-    min-height: 100vh;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     z-index: 1000001;
   }
 
