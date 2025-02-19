RevenueCat/purchases-js

commit 9bb0977124db4fe0036a9ad3c2c1e2cf505d4d18

Re-added support to rendering the sdk in an element passed as parameter (#324)
Adds support for passing an HTML Element and have the purchase flow rendered there instead of taking the whole screen

src/main.ts
@@ -595,6 +595,7 @@ export class Purchases {
       mount(RCPurchasesUI, {
         target: certainHTMLTarget,
         props: {
+          isInElement: htmlTarget !== undefined,
           appUserId,
           rcPackage,
           purchaseOption: purchaseOptionToUse,

src/stories/purchase-flow-desktop-embedded.stories.svelte
@@ -1,0 +1,193 @@
+<script module>
+  import { defineMeta, setTemplate } from "@storybook/addon-svelte-csf";
+  import WithContext from "./utils/with-context.svelte";
+  import { toProductInfoStyleVar } from "../ui/theme/utils";
+  import RcbUiInner from "../ui/rcb-ui-inner.svelte";
+  import { translatorContextKey } from "../ui/localization/constants";
+  import { Translator } from "../ui/localization/translator";
+  import {
+    brandingInfo,
+    colorfulBrandingAppearance,
+    product,
+    purchaseFlowError,
+    subscriptionOption,
+    subscriptionOptionWithTrial,
+  } from "./fixtures";
+  import { buildCheckoutStartResponse } from "./utils/purchase-response-builder";
+  import { type CheckoutStartResponse } from "../networking/responses/checkout-start-response";
+
+  const defaultArgs = {
+    context: {},
+    productDetails: product,
+    purchaseOptionToUse: subscriptionOption,
+    purchaseOption: subscriptionOption,
+    brandingInfo: { ...brandingInfo, appearance: colorfulBrandingAppearance },
+    lastError: purchaseFlowError,
+    onContinue: () => {},
+  };
+
+  let paymentMetadata: any;
+
+  let { Story } = defineMeta({
+    title: "Purchase flow (Desktop Embedded)",
+    args: defaultArgs,
+    parameters: {},
+    loaders: [
+      async () => {
+        const paymentInfoCollectionMetadata: CheckoutStartResponse =
+          await buildCheckoutStartResponse();
+        paymentMetadata = { ...paymentInfoCollectionMetadata };
+        return { paymentInfoCollectionMetadata };
+      },
+    ],
+  });
+
+  // @ts-ignore
+  let colorVariables = toProductInfoStyleVar(brandingInfo?.appearance);
+</script>
+
+<script lang="ts">
+  import { eventsTrackerContextKey } from "../ui/constants";
+
+  setTemplate(template);
+</script>
+
+{#snippet template(args: any)}
+  <div
+    id="embedding-container"
+    style="width: 500px; height: 600px; position: relative; overflow: hidden; background-color: lightgray;"
+  >
+    <WithContext
+      context={{
+        ...args.context,
+        [translatorContextKey]: args.locale
+          ? new Translator({}, args.locale, args.locale)
+          : undefined,
+        [eventsTrackerContextKey]: { trackSDKEvent: () => {} },
+      }}
+    >
+      <RcbUiInner
+        {...args}
+        {colorVariables}
+        paymentInfoCollectionMetadata={paymentMetadata}
+      />
+    </WithContext>
+  </div>
+{/snippet}
+
+<Story
+  name="Email Input"
+  args={{ currentView: "needs-auth-info" }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Email Input (with Sandbox Banner)"
+  args={{
+    currentView: "needs-auth-info",
+    isSandbox: true,
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Email Input (with Trial Product)"
+  args={{
+    currentView: "needs-auth-info",
+    isSandbox: true,
+    productDetails: {
+      ...product,
+      normalPeriodDuration: "P1Y",
+    },
+    purchaseOptionToUse: subscriptionOptionWithTrial,
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Checkout"
+  args={{
+    ...defaultArgs,
+    currentView: "needs-payment-info",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Loading"
+  args={{
+    currentView: "loading",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Payment complete"
+  args={{
+    currentView: "success",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Payment failed"
+  args={{
+    currentView: "error",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Email Input (Arabic)"
+  args={{ currentView: "needs-auth-info", locale: "ar" }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Email Input (with Sandbox Banner) (Arabic)"
+  args={{
+    currentView: "needs-auth-info",
+    isSandbox: true,
+    locale: "ar",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Email Input (with Trial Product) (Arabic)"
+  args={{
+    currentView: "needs-auth-info",
+    isSandbox: true,
+    locale: "ar",
+    productDetails: {
+      ...product,
+      normalPeriodDuration: "P1Y",
+    },
+    purchaseOptionToUse: subscriptionOptionWithTrial,
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Checkout (Arabic)"
+  args={{
+    ...defaultArgs,
+    currentView: "needs-payment-info",
+    locale: "ar",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Loading (Arabic)"
+  args={{
+    currentView: "loading",
+    locale: "ar",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Payment complete (Arabic)"
+  args={{
+    currentView: "success",
+    locale: "ar",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>
+<Story
+  name="Payment failed (Arabic)"
+  args={{
+    currentView: "error",
+    locale: "ar",
+  }}
+  parameters={{ viewport: { defaultViewport: "desktop" } }}
+/>

src/ui/layout/container.svelte
@@ -20,10 +20,12 @@
     display: flex;
     z-index: 1000001;
     flex-direction: column;
-    position: fixed;
+    position: absolute;
+    top: 0;
+    left: 0;
     inset: 0;
-    width: 100vw;
-    min-height: 100vh;
+    width: 100%;
+    min-height: 100%;
     color-scheme: none;
     font-size: 16px;
     line-height: 1.5em;

src/ui/rcb-ui.svelte
@@ -46,6 +46,7 @@
   export let selectedLocale: string = englishLocale;
   export let defaultLocale: string = englishLocale;
   export let customTranslations: CustomTranslations = {};
+  export let isInElement: boolean = false;
 
   let colorVariables = "";
   let productDetails: Product | null = null;
@@ -64,11 +65,15 @@
   );
 
   onMount(() => {
-    document.body.style.overflow = "hidden"; // Prevents background scrolling
+    if (!isInElement) {
+      document.body.style.overflow = "hidden"; // Prevents background scrolling
+    }
   });
 
   onDestroy(() => {
-    document.body.style.overflow = ""; // Restores default scrolling when unmounting
+    if (!isInElement) {
+      document.body.style.overflow = ""; // Restores default scrolling when unmounting
+    }
   });
 
   setContext(eventsTrackerContextKey, eventsTracker);

src/ui/badge.svelte
@@ -27,7 +27,7 @@
     text-transform: uppercase;
   }
 
-  @media screen and (min-width: 768px) {
+  @container layout-query-container (width >= 768px) {
     .badge {
       padding: var(--rc-spacing-gapSmall-desktop) 16px;
       font: var(--rc-text-caption-desktop);

src/ui/button.svelte
@@ -36,7 +36,7 @@
     transition: background-color 0.15s ease-in-out;
   }
 
-  @media screen and (min-width: 768px) {
+  @container layout-query-container (width >= 768px) {
     button {
       height: var(--rc-spacing-inputHeight-desktop);
     }

src/ui/layout/layout.svelte
@@ -5,25 +5,36 @@
   export let children: Snippet;
 </script>
 
-<div class="rcb-ui-layout" {style}>
-  {@render children?.()}
+<div id="layout-query-container">
+  <div class="rcb-ui-layout" {style}>
+    {@render children?.()}
+  </div>
 </div>
 
 <style>
+  #layout-query-container {
+    width: 100%;
+    height: 100%;
+    container-type: size;
+    container-name: layout-query-container;
+  }
+
   .rcb-ui-layout {
-    width: 100vw;
+    width: 100%;
+    min-height: 100%;
     display: flex;
     box-sizing: border-box;
     flex-direction: column;
   }
 
-  @media screen and (max-width: 767px) {
+  /* Levaraging container queries */
+  @container layout-query-container (width < 768px) {
     .rcb-ui-layout {
       flex-grow: 1;
     }
   }
 
-  @media screen and (min-width: 768px) {
+  @container layout-query-container (width >= 768px) {
     .rcb-ui-layout {
       flex-direction: row;
     }

src/ui/layout/message-layout.svelte
@@ -88,13 +88,13 @@
     font: var(--rc-text-body1-mobile);
   }
 
-  @media (max-width: 767px) {
+  @container layout-query-container (width < 768px) {
     .message-layout {
       flex-grow: 1;
     }
   }
 
-  @media (min-width: 768px) {
+  @container layout-query-container (width >= 768px) {
     .message-layout-content {
       justify-content: flex-start;
       flex-grow: 0;

commit 86860f4033565ee07f9ff1638620e9d565bd786e

[WEB-2140] restore scroll after closing modal + fixing height (#326)
This PR:

- Fixes issue when closing modal. `onDestroy` is not being called so
scroll is not returned to the app
- Fixes issue introduced by
https://github.com/RevenueCat/purchases-js/pull/324/files#diff-890948e5715657ae559e6221fa767a9c6c771e193ddc37fc42add8c8ab491e6eL23
whereby the modal won't take the full screen when having scrolled down
(since it's absolute 100%/100%). It fixes the issue by returning
conditionally the previous styles, based on `isInElement`

.env.development
@@ -1,3 +1,3 @@
-VITE_RC_ENDPOINT=http://localhost:8000
+VITE_RC_ENDPOINT=https://api.revenuecat.com
 VITE_RC_ANALYTICS_ENDPOINT=https://localhost:8000
 VITE_ASSETS_ENDPOINT=https://da08ctfrofx1b.cloudfront.net

src/main.ts
@@ -43,7 +43,7 @@ import {
 } from "./helpers/offerings-parser";
 import { type RedemptionInfo } from "./entities/redemption-info";
 import { type PurchaseResult } from "./entities/purchase-result";
-import { mount } from "svelte";
+import { mount, unmount } from "svelte";
 import { type RenderPaywallParams } from "./entities/render-paywall-params";
 import { Paywall } from "@revenuecat/purchases-ui-js";
 import { PaywallDefaultContainerZIndex } from "./ui/theme/constants";
@@ -580,6 +580,8 @@ export class Purchases {
       : {};
     const metadata = { ...utmParamsMetadata, ...(params.metadata || {}) };
 
+    let component: ReturnType<typeof mount> | null = null;
+
     const finalBrandingInfo: BrandingInfoResponse | null = this._brandingInfo;
 
     if (finalBrandingInfo && params.brandingAppearanceOverride) {
@@ -592,14 +594,20 @@ export class Purchases {
         const event = createCheckoutSessionEndClosedEvent();
         this.eventsTracker.trackSDKEvent(event);
         window.removeEventListener("popstate", onClose);
+
+        if (component) {
+          unmount(component);
+        }
+
         certainHTMLTarget.innerHTML = "";
+
         Logger.debugLog("Purchase cancelled by user");
         reject(new PurchasesError(ErrorCode.UserCancelledError));
       };
 
       window.addEventListener("popstate", onClose);
 
-      mount(RCPurchasesUI, {
+      component = mount(RCPurchasesUI, {
         target: certainHTMLTarget,
         props: {
           isInElement: htmlTarget !== undefined,

src/stories/purchase-flow-desktop-embedded.stories.svelte
@@ -69,6 +69,7 @@
       <RcbUiInner
         {...args}
         {colorVariables}
+        isInElement={true}
         paymentInfoCollectionMetadata={paymentMetadata}
       />
     </WithContext>

src/ui/layout/container.svelte
@@ -9,10 +9,17 @@
   let spacingStyle = new Theme(brandingAppearance).spacingStyleVars;
   let style = [textStyle, spacingStyle].join("; ");
 
+  export let isInElement: boolean = false;
+
   export let children: Snippet;
 </script>
 
-<div class="rcb-ui-container" {style}>
+<div
+  class="rcb-ui-container"
+  class:fullscreen={!isInElement}
+  class:inside={isInElement}
+  {style}
+>
   {@render children?.()}
 </div>
 
@@ -21,12 +28,7 @@
     display: flex;
     z-index: 1000001;
     flex-direction: column;
-    position: absolute;
-    top: 0;
-    left: 0;
     inset: 0;
-    width: 100%;
-    min-height: 100%;
     color-scheme: none;
     font-size: 16px;
     line-height: 1.5em;
@@ -46,5 +48,19 @@
       arial,
       sans-serif;
     overflow: auto;
+  }
+
+  .rcb-ui-container.fullscreen {
+    position: fixed;
+    width: 100vw;
+    min-height: 100vh;
+  }
+
+  .rcb-ui-container.inside {
+    position: absolute;
+    width: 100%;
+    min-height: 100%;
+    top: 0;
+    left: 0;
   }
 </style>

src/ui/rcb-ui-inner.svelte
@@ -28,6 +28,7 @@
   export let purchaseOptionToUse: PurchaseOption;
   export let colorVariables: string = "";
   export let isSandbox: boolean = false;
+  export let isInElement: boolean = false;
 
   export let handleContinue: (params?: ContinueHandlerParams) => void;
   export let closeWithError: () => void;
@@ -47,7 +48,7 @@
   ];
 </script>
 
-<Container brandingAppearance={brandingInfo?.appearance}>
+<Container brandingAppearance={brandingInfo?.appearance} {isInElement}>
   {#if isSandbox}
     <SandboxBanner style={colorVariables} />
   {/if}

src/ui/rcb-ui.svelte
@@ -210,4 +210,5 @@
   {purchaseOperationHelper}
   {closeWithError}
   {colorVariables}
+  {isInElement}
 />

src/tests/main.test.ts
@@ -1,16 +1,20 @@
 import { describe, expect, test, vi } from "vitest";
+import * as svelte from "svelte"; // import the module as a namespace
+
 import {
   type CustomerInfo,
   type EntitlementInfo,
   Purchases,
   PurchasesError,
 } from "../main";
-import { UninitializedPurchasesError } from "../entities/errors";
+import { ErrorCode, UninitializedPurchasesError } from "../entities/errors";
 import {
   configurePurchases,
   testApiKey,
   testUserId,
 } from "./base.purchases_test";
+import { createMonthlyPackageMock } from "./mocks/offering-mock-provider";
+import { waitFor } from "@testing-library/svelte";
 
 describe("Purchases.configure()", () => {
   test("throws error if given invalid api key", () => {
@@ -241,5 +245,38 @@ describe("Purchases._trackEvent", () => {
         test_property: "test_value",
       },
     });
+  });
+});
+
+describe("Purchases.purchase()", () => {
+  test("pressing back button onmounts the component", async () => {
+    const unmountSpy = vi.spyOn(svelte, "unmount").mockImplementation(() => {
+      return Promise.resolve();
+    });
+
+    const purchases = configurePurchases();
+    const purchasePromise = purchases.purchase({
+      rcPackage: createMonthlyPackageMock(),
+    });
+
+    await waitFor(() => {
+      const container = document.querySelector(".rcb-ui-root");
+      expect(container).not.toBeNull();
+      if (container) {
+        expect(container.innerHTML).not.toBe("");
+      }
+    });
+
+    window.dispatchEvent(new PopStateEvent("popstate"));
+    await waitFor(() => {
+      expect(unmountSpy).toHaveBeenCalled();
+    });
+
+    await expect(purchasePromise).rejects.toHaveProperty(
+      "errorCode",
+      ErrorCode.UserCancelledError,
+    );
+
+    unmountSpy.mockRestore();
   });
 });