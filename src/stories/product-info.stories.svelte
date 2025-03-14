df816c48737b13a83b687648cc3ff1a2754aba9b
Pol Miro <polmiro@gmail.com>
2025-03-14 14:43:00 +0100

    Add ProductInfo stories

---
+++ src/stories/product-info.stories.svelte
@@ -0,0 +1,41 @@
+<script module>
+  import { brandingLanguageViewportModes } from "../../.storybook/modes";
+  import { defineMeta, setTemplate } from "@storybook/addon-svelte-csf";
+  import { withNavbarDecorator } from "./utils/decorators";
+  import ProductInfo from "../ui/components/product-info.svelte";
+  import {
+    product,
+    subscriptionOption,
+    subscriptionOptionWithTrial,
+    nonSubscriptionOption,
+    consumableProduct,
+  } from "./fixtures";
+
+  const { Story } = defineMeta({
+    component: ProductInfo,
+    title: "Components/ProductInfo",
+    // @ts-expect-error ignore typing of decorator
+    decorators: [withNavbarDecorator],
+    parameters: {
+      chromatic: {
+        modes: brandingLanguageViewportModes,
+      },
+    },
+  });
+</script>
+
+<Story name="Subscription With Description">
+  <ProductInfo
+    productDetails={product}
+    purchaseOption={subscriptionOption}
+    showProductDescription={true}
+  />
+</Story>
+
+<Story name="Non Subscription With Description">
+  <ProductInfo
+    productDetails={consumableProduct}
+    purchaseOption={nonSubscriptionOption}
+    showProductDescription={true}
+  />
+</Story>
