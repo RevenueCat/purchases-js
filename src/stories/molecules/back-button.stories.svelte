a4db224c539a9d55f02988c1cd7bf88f8ba89d08
Pol Miro <polmiro@gmail.com>
2025-03-17 10:08:59 +0100

    Add story for BackButton

---
+++ src/stories/molecules/back-button.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+  import BackButton from "../../ui/molecules/back-button.svelte";
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { withLayout } from "../decorators/with-layout";
+  import { brandingModes } from "../../../.storybook/modes";
+
+  let { Story } = defineMeta({
+    component: BackButton,
+    title: "Molecules/BackButton",
+    // @ts-expect-error ignore typing of decorator
+    decorators: [withLayout],
+    parameters: {
+      chromatic: {
+        modes: brandingModes,
+      },
+    },
+  });
+</script>
+
+<Story name="Default" />
+<Story name="Disabled" args={{ disabled: true }} />
