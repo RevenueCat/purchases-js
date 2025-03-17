8dad3da80190006f24791050eeb99dbdf6ede6a4
Pol Miro <polmiro@gmail.com>
2025-03-17 12:49:11 +0100

    Add Dropdown molecule

--- src/ui/localization/supportedLanguages.ts
+++ src/ui/localization/supportedLanguages.ts
@@ -96,6 +96,8 @@ export enum LocalizationKeys {
   PaywallVariablesPricePerPeriod = "paywall_variables.price_per_period",
   PaywallVariablesSubRelativeDiscount = "paywall_variables.sub_relative_discount",
   PaywallVariablesTotalPriceAndPerMonth = "paywall_variables.total_price_and_per_month",
+  PricingDropdownShowDetails = "pricing_dropdown.show_details",
+  PricingDropdownHideDetails = "pricing_dropdown.hide_details",
   NavbarHeaderDetails = "navbar_header.details",
   NavbarBackButton = "navbar_header.back_button",
 }
