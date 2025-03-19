80d8018b15433f4b0441be778e27a2b6dcecad87
Pol Miro <polmiro@gmail.com>
2025-03-19 17:06:12 +0100

    Remove modes for TaxTitle

--- src/stories/atoms/tax-title.stories.svelte
+++ src/stories/atoms/tax-title.stories.svelte
@@ -2,18 +2,12 @@
   import TaxTitle from "../../ui/atoms/tax-title.svelte";
   import { defineMeta, setTemplate } from "@storybook/addon-svelte-csf";
   import { withLayout } from "../decorators/with-layout";
-  import { brandingModes } from "../../../.storybook/modes";
 
   let { Story } = defineMeta({
     component: TaxTitle,
     title: "Atoms/TaxTitle",
     // @ts-expect-error ignore typing of decorator
     decorators: [withLayout],
-    parameters: {
-      chromatic: {
-        modes: brandingModes,
-      },
-    },
   });
 </script>
 
