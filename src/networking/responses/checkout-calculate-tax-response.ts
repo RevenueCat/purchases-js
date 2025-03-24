cd6c53d2a337404e6fd27eb8b995b50673a5c7ae
Pol Miro <polmiro@gmail.com>
2025-03-24 16:47:58 +0100

    chore: Represent the tax calculation in the UI

--- src/networking/responses/checkout-calculate-tax-response.ts
+++ src/networking/responses/checkout-calculate-tax-response.ts
@@ -7,11 +7,15 @@ export interface TaxBreakdown {
   country: string | null;
   state: string | null;
   tax_type: string | null;
+  display_name: string | null;
 }
 
 export interface CheckoutCalculateTaxResponse {
   operation_session_id: string;
   currency: string;
+  total_amount_in_micros: number;
+  tax_amount_in_micros: number;
+  total_excluding_tax_in_micros: number;
   tax_inclusive: boolean;
   pricing_phases: {
     base: {
