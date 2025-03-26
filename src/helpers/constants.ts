7a9b9eb4be283f9f95089d854ec10e82116260d0
vicfergar <victor@revenuecat.com>
2025-03-26 12:26:32 +0100

    chore: Put tax calculations behind the `ALLOW_TAX_CALCULATION_FF` feature flag

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -2,3 +2,5 @@ export const VERSION = "1.0.5";
 export const RC_ENDPOINT = import.meta.env.VITE_RC_ENDPOINT as string;
 export const RC_ANALYTICS_ENDPOINT = import.meta.env
   .VITE_RC_ANALYTICS_ENDPOINT as string;
+export const ALLOW_TAX_CALCULATION_FF =
+  import.meta.env.VITE_ALLOW_TAX_CALCULATION_FF === "true";
