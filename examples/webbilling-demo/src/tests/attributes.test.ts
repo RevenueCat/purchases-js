44d0cd3ce6211e6a474aa0e490c38bb93371b68c
Antonio Borrero Granell <antonio@revenuecat.com>
2025-03-31 15:33:39 +0200

    Fix e2e test

--- examples/webbilling-demo/src/tests/attributes.test.ts
+++ examples/webbilling-demo/src/tests/attributes.test.ts
@@ -19,6 +19,26 @@ integrationTest("Can set user attributes", async ({ page, userId }) => {
   const request = await requestPromise;
   const requestBody = request.postDataJSON();
 
-  expect(requestBody).toHaveProperty("$displayName", "Test User");
-  expect(requestBody).toHaveProperty("nickname", "testy");
+  // Verify the request payload
+  expect(requestBody).toHaveProperty("attributes");
+  expect(requestBody.attributes).toHaveProperty("$displayName");
+  expect(requestBody.attributes).toHaveProperty("nickname");
+
+  expect(requestBody.attributes["$displayName"]).toHaveProperty(
+    "value",
+    "Test User",
+  );
+  expect(requestBody.attributes["$displayName"]).toHaveProperty(
+    "updated_at_ms",
+  );
+  expect(requestBody.attributes["nickname"]).toHaveProperty("value", "testy");
+  expect(requestBody.attributes["nickname"]).toHaveProperty("updated_at_ms");
+
+  // Verify the request succeeds by waiting for the response
+  const response = await request.response();
+  expect(response?.status()).toBe(200);
+
+  // Verify the response body is empty
+  const responseBody = await response?.json();
+  expect(responseBody).toEqual({});
 });
