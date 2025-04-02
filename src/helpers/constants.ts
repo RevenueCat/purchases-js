e45c19341929ea2c87755dc75e67fe499ec8444c
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-04-02 18:01:43 +0000

    Version bump for 1.0.6

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,4 @@
-export const VERSION = "1.0.5";
+export const VERSION = "1.0.6";
 export const RC_ENDPOINT = import.meta.env.VITE_RC_ENDPOINT as string;
 export const RC_ANALYTICS_ENDPOINT = import.meta.env
   .VITE_RC_ANALYTICS_ENDPOINT as string;
