791b1d8182c263d2576918eb57473d9b82d99f2c
Pol Miro <polmiro@gmail.com>
2025-04-11 21:43:38 +0200

    Overhaul refactor integration tests (#437)
    ## Motivation / Description
    
    - Grouped integration tests into different files.
    - Extracted fixtures.
    - Extracted helpers.
    - Extracted a `integrationTest` to always make available a page, userId
    and email.
    - Split some helpers into more natural steps, e.g.:
    `enterCreditCardDetails`, `clickPayButton`, `clickContinueButton`,
    `getPackageCards`, `getPaywallPackageCards`, etc.

---
+++ examples/webbilling-demo/src/tests/event-tracking.test.ts
@@ -0,0 +1,69 @@
+import type { Response } from "@playwright/test";
+import { expect } from "@playwright/test";
+import { navigateToLandingUrl } from "./helpers/test-helpers";
+import { integrationTest } from "./helpers/integration-test";
+
+integrationTest("Tracks events", async ({ page, userId }) => {
+  page = await navigateToLandingUrl(page, userId);
+
+  const waitForTrackEventPromise = page.waitForResponse(
+    successfulEventTrackingResponseMatcher((event) => {
+      try {
+        expect(event?.id).toBeDefined();
+        expect(event?.timestamp_ms).toBeDefined();
+        expect(event?.type).toBe("web_billing");
+        expect(event?.event_name).toBe("sdk_initialized");
+        expect(event?.app_user_id).toBe(userId);
+
+        const context = event?.context;
+        expect(context).toBeInstanceOf(Object);
+
+        expect(context.library_name).toEqual("purchases-js");
+        expect(typeof context.library_version).toBe("string");
+        expect(typeof context.locale).toBe("string");
+        expect(typeof context.user_agent).toBe("string");
+        expect(typeof context.time_zone).toBe("string");
+        expect(typeof context.screen_width).toBe("number");
+        expect(typeof context.screen_height).toBe("number");
+        expect(context.utm_source).toBeNull();
+        expect(context.utm_medium).toBeNull();
+        expect(context.utm_campaign).toBeNull();
+        expect(context.utm_content).toBeNull();
+        expect(context.utm_term).toBeNull();
+        expect(context.page_referrer).toBe("");
+        expect(typeof context.page_url).toBe("string");
+        expect(context.page_title).toBe("Health Check – Web Billing Demo");
+        expect(context.source).toBe("sdk");
+
+        const properties = event?.properties;
+        expect(typeof properties.trace_id).toBe("string");
+
+        return true;
+      } catch (error) {
+        console.error("Event validation failed:", error);
+        return false;
+      }
+    }),
+    { timeout: 3_000 },
+  );
+  await navigateToLandingUrl(page, userId);
+  await waitForTrackEventPromise;
+});
+
+function successfulEventTrackingResponseMatcher(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  eventMatcher: (event: any) => boolean,
+) {
+  return async (response: Response) => {
+    if (
+      response.url() !== "https://e.revenue.cat/v1/events" ||
+      response.status() !== 200
+    ) {
+      return false;
+    }
+
+    const json = response.request().postDataJSON();
+    const sdk_initialized_events = (json?.events || []).filter(eventMatcher);
+    return sdk_initialized_events.length === 1;
+  };
+}
---
+++ examples/webbilling-demo/src/tests/localization.test.ts
@@ -0,0 +1,53 @@
+import { expect } from "@playwright/test";
+import {
+  getPackageCards,
+  getPaywallPackageCards,
+  startPurchaseFlow,
+  navigateToLandingUrl,
+  getPaywallPurchaseButtons,
+} from "./helpers/test-helpers";
+import {
+  integrationTest,
+  skipPaywallsTestIfDisabled,
+} from "./helpers/integration-test";
+import { RC_PAYWALL_TEST_OFFERING_ID } from "./helpers/fixtures";
+
+const TEST_CASES = [
+  ["es", "Pago seguro mediante RevenueCat"],
+  ["it", "Pagamento sicuro tramite RevenueCat"],
+  ["en", "Secure checkout by RevenueCat"],
+  ["fr", "Paiement sécurisé par RevenueCat"],
+  ["de", "Sicherer Checkout über RevenueCat"],
+];
+
+TEST_CASES.forEach(([lang, title]) => {
+  integrationTest(`Displays in ${lang}`, async ({ page, userId }) => {
+    page = await navigateToLandingUrl(page, userId, { lang });
+
+    const packageCards = await getPackageCards(page);
+    await startPurchaseFlow(packageCards[1]);
+
+    await expect(page.getByText(title)).toBeVisible({ timeout: 10000 });
+  });
+
+  integrationTest(`Displays paywall in ${lang}`, async ({ page, userId }) => {
+    skipPaywallsTestIfDisabled(integrationTest);
+
+    page = await navigateToLandingUrl(page, userId, {
+      lang,
+      offeringId: RC_PAYWALL_TEST_OFFERING_ID,
+      useRcPaywall: true,
+    });
+
+    const packageCards = await getPaywallPackageCards(page);
+    await packageCards[0].click();
+
+    const purchaseButtons = await getPaywallPurchaseButtons(page);
+    const purchaseButton = purchaseButtons[0];
+
+    await expect(purchaseButton).toBeVisible();
+    await purchaseButton.click();
+
+    await expect(page.getByText(title)).toBeVisible({ timeout: 10000 });
+  });
+});
