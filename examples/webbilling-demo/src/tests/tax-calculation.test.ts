7ed5cacdcd21f01237a159d82af10e8c8b8820d8
vicfergar <victor@revenuecat.com>
2025-04-11 13:54:28 +0200

    chore: Add test for not-collecting jurisdictions

--- examples/webbilling-demo/src/tests/tax-calculation.test.ts
+++ examples/webbilling-demo/src/tests/tax-calculation.test.ts
@@ -118,6 +118,45 @@ test.describe("Tax calculation breakdown", () => {
     await expect(page.getByText("Total due today")).toBeVisible();
   });
 
+  test("should not display the tax breakdown if not collecting location is selected", async ({
+    browser,
+    browserName,
+  }) => {
+    const userId = getUserId(browserName);
+
+    // Set up the page (standard user, location, default params)
+    const page = await setupTest(
+      browser,
+      userId,
+      {
+        offeringId: TAXES_TEST_OFFERING_ID,
+      },
+      TAX_TEST_API_KEY,
+    );
+
+    // Select and make a purchase of the first available product card
+    const cards = await getAllElementsByLocator(page, CARD_SELECTOR);
+    const targetCard = cards[0];
+
+    // Perform a purchase action
+    await startPurchaseFlow(targetCard);
+    await enterEmailAndContinue(page, userId);
+
+    // Expecting the first calculation to be done.
+    await expect(page.getByText("Total excluding tax")).toBeVisible();
+    await expect(page.getByText(/Sales Tax - New York/)).not.toBeVisible();
+    await expect(page.getByText("Total due today")).toBeVisible();
+
+    await enterCreditCardDetails(page, "4242 4242 4242 4242", {
+      countryCode: "US",
+      postalCode: "33125", // Miami, FL
+    });
+
+    await expect(page.getByText("Total excluding tax")).not.toBeVisible();
+    await expect(page.getByText(/Sales Tax - New York/)).not.toBeVisible();
+    await expect(page.getByText("Total due today")).toBeVisible();
+  });
+
   test("product price and total should match for inclusive taxes", async ({
     browser,
     browserName,
