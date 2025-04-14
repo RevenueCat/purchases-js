81321154d00a9bb4acf964686702dc5e47fc4236
Pol Miro <polmiro@gmail.com>
2025-04-14 14:33:32 -0700

    Avoid running tracking events tests in development

--- examples/webbilling-demo/src/tests/helpers/fixtures.ts
+++ examples/webbilling-demo/src/tests/helpers/fixtures.ts
@@ -6,5 +6,9 @@ export const TAX_TEST_API_KEY = process.env.VITE_RC_TAX_E2E_API_KEY;
 export const TAX_TEST_OFFERING_ID = "rcb_e2e_taxes";
 export const LOCAL_URL = "http://localhost:3001/";
 export const BASE_URL =
-  (import.meta.env?.VITE_RC_BILLING_DEMO_URL as string | undefined) ??
-  LOCAL_URL;
+  (process.env?.VITE_RC_BILLING_DEMO_URL as string | undefined) ?? LOCAL_URL;
+export const VITE_RC_ENDPOINT = process.env.VITE_RC_ENDPOINT as
+  | string
+  | undefined;
+export const VITE_RC_ANALYTICS_ENDPOINT = process.env
+  ?.VITE_RC_ANALYTICS_ENDPOINT as string | undefined;
