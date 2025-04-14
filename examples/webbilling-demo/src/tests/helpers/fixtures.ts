08b9e721f0b793a8ca03421352d82cde53b9b9cd
Pol Miro <polmiro@gmail.com>
2025-04-14 21:32:40 +0200

    Add an env var for non tax test api key (#443)
    ## Motivation / Description
    
    Prevents collisions with development environment variable.

--- examples/webbilling-demo/src/tests/helpers/fixtures.ts
+++ examples/webbilling-demo/src/tests/helpers/fixtures.ts
@@ -1,8 +1,9 @@
 export const RC_PAYWALL_TEST_OFFERING_ID = "rc_paywalls_e2e_test_2";
 export const RC_PAYWALL_TEST_OFFERING_ID_WITH_VARIABLES =
   "rc_paywalls_e2e_test_variables_2";
-export const TAX_TEST_OFFERING_ID = "rcb_e2e_taxes";
+export const NON_TAX_TEST_API_KEY = process.env.VITE_RC_NON_TAX_E2E_API_KEY;
 export const TAX_TEST_API_KEY = process.env.VITE_RC_TAX_E2E_API_KEY;
+export const TAX_TEST_OFFERING_ID = "rcb_e2e_taxes";
 export const LOCAL_URL = "http://localhost:3001/";
 export const BASE_URL =
   (import.meta.env?.VITE_RC_BILLING_DEMO_URL as string | undefined) ??
