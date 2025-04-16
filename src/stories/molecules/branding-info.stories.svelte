7f98bf65d31802c597ac448495dcd1f4b186708a
Pol Miro <polmiro@gmail.com>
2025-04-16 22:29:49 +0200

    Add stories for BrandingInfo (#448)
    ## Motivation / Description
    
    Add stories for BrandingInfo prior to making changes to it to review
    them easily in Chromatic.

---
+++ src/stories/molecules/branding-info.stories.svelte
@@ -0,0 +1,32 @@
+<script module>
+  import BrandingInfo from "../../ui/molecules/branding-info.svelte";
+  import {
+    type Args,
+    defineMeta,
+    setTemplate,
+    type StoryContext,
+  } from "@storybook/addon-svelte-csf";
+  import { brandingInfos } from "../fixtures";
+  import { withNavbar } from "../decorators/with-navbar";
+
+  let { Story } = defineMeta({
+    component: BrandingInfo,
+    // @ts-expect-error ignore typing of decorator
+    decorators: [withNavbar],
+    title: "Molecules/Branding Info",
+  });
+</script>
+
+<script lang="ts">
+  setTemplate(template);
+</script>
+
+{#snippet template(
+  _args: Args<typeof Story>,
+  context: StoryContext<typeof Story>,
+)}
+  {@const brandingInfo = brandingInfos[context.globals.brandingName]}
+  <BrandingInfo {brandingInfo} />
+{/snippet}
+
+<Story name="Default" />
