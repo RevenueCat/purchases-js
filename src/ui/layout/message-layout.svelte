8fdda71ca4db82aa0e102f3d1874a13e228ec0bf
Pol Miro <polmiro@gmail.com>
2025-04-17 15:15:33 -0700

    Update MessageLayout to use Typography

--- src/ui/layout/message-layout.svelte
+++ src/ui/layout/message-layout.svelte
@@ -4,6 +4,7 @@
   import ModalSection from "./modal-section.svelte";
   import RowLayout from "./row-layout.svelte";
   import { type ContinueHandlerParams } from "../ui-types";
+  import Typography from "../atoms/typography.svelte";
 
   export let onContinue: (params?: ContinueHandlerParams) => void;
   export let title: string | null = null;
@@ -32,12 +33,10 @@
                 {@render icon()}
               {/if}
               {#if title}
-                <span class="rcb-title">{title}</span>
+                <Typography size="heading-lg">{title}</Typography>
               {/if}
               {#if message}
-                <span class="rcb-subtitle">
-                  {@render message()}
-                </span>
+                <Typography size="body-base">{@render message?.()}</Typography>
               {/if}
             </RowLayout>
           </RowLayout>
@@ -80,14 +79,6 @@
     margin-top: var(--rc-spacing-gapXXLarge-mobile);
   }
 
-  .rcb-title {
-    font: var(--rc-text-titleLarge-mobile);
-  }
-
-  .rcb-subtitle {
-    font: var(--rc-text-body1-mobile);
-  }
-
   @container layout-query-container (width < 768px) {
     .message-layout {
       flex-grow: 1;
@@ -108,14 +99,6 @@
     }
     .rcb-modal-message[data-has-title="false"] {
       margin-top: var(--rc-spacing-gapXXLarge-desktop);
-    }
-
-    .rcb-title {
-      font: var(--rc-text-titleLarge-desktop);
-    }
-
-    .rcb-subtitle {
-      font: var(--rc-text-body1-desktop);
     }
   }
 </style>
