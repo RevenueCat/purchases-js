46d6bce141d60051854a4cddc77c42d7b9c1bc3f
Alfonso Embid-Desmet <alfperata@gmail.com>
2025-05-05 18:41:46 -0600

    render wordmark if possible

---
+++ src/ui/atoms/app-wordmark.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  type Props = {
+    src: string | null;
+    srcWebp: string | null;
+  };
+
+  const { src, srcWebp }: Props = $props();
+</script>
+
+{#if src}
+  <picture class="rcb-app-icon-picture-container">
+    {#if srcWebp}
+      <source type="image/webp" srcset={srcWebp} />
+    {/if}
+    <img class="rcb-app-icon" {src} alt="App icon" />
+  </picture>
+{:else}
+  <div class="rcb-app-wordmark loading"></div>
+{/if}
+
+<style>
+  .rcb-app-wordmark {
+    width: 100%;
+    height: 32px;
+    box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.1);
+  }
+
+  .rcb-app-icon-picture-container {
+    height: 32px;
+  }
+
+  .rcb-app-wordmark.loading {
+    background-color: gray;
+  }
+</style>
