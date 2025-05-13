50a0bf659790632da7adc0165fe18f320bd7d9f0
Roger Solé <roger.sole@gmail.com>
2025-05-13 09:28:50 +0200

    changes to handle paddle web purchase

--- src/helpers/configuration-validators.ts
+++ src/helpers/configuration-validators.ts
@@ -2,8 +2,9 @@ import { ErrorCode, PurchasesError } from "../entities/errors";
 import { SDK_HEADERS } from "../networking/http-client";
 
 export function validateApiKey(apiKey: string) {
-  const api_key_regex = /^rcb_[a-zA-Z0-9_.-]+$/;
-  if (!api_key_regex.test(apiKey)) {
+  const rc_api_key_regex = /^rcb_[a-zA-Z0-9_.-]+$/;
+  const paddle_api_key_regex = /^pdl_[a-zA-Z0-9_.-]+$/;
+  if (!rc_api_key_regex.test(apiKey) && !paddle_api_key_regex.test(apiKey)) {
     throw new PurchasesError(
       ErrorCode.InvalidCredentialsError,
       "Invalid API key. Use your Web Billing API key.",
