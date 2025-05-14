b141b6cc74467aed4860950d203d7243880af793
Pol Miro <polmiro@gmail.com>
2025-05-14 12:10:10 +0200

    Remove arabic from stories

--- .storybook/modes.ts
+++ .storybook/modes.ts
@@ -35,39 +35,18 @@ export const brandingLanguageViewportModes = {
     brandingName: "Igify",
     locale: "fr",
   },
-  "mobile-ar-dipsea": {
-    name: "Mobile / Arabic / Dipsea",
-    viewport: "mobile",
-    brandingName: "Dipsea",
-    locale: "ar",
-  },
-  "desktop-ar-dipsea": {
-    name: "Desktop / Arabic / Dipsea",
-    viewport: "desktop",
-    brandingName: "Dipsea",
-    locale: "ar",
-  },
-  "embedded-ar-dipsea": {
-    name: "Embedded / Arabic / Dipsea",
-    viewport: "embedded",
-    brandingName: "Dipsea",
-    locale: "ar",
-  },
 };
 
 export const brandingModes = {
   "mobile-en-default": brandingLanguageViewportModes["mobile-en-default"],
   "mobile-fr-igify": brandingLanguageViewportModes["mobile-fr-igify"],
-  "mobile-ar-dipsea": brandingLanguageViewportModes["mobile-ar-dipsea"],
 };
 
 export const mobileAndDesktopBrandingModes = {
   "mobile-en-default": brandingLanguageViewportModes["mobile-en-default"],
   "mobile-fr-igify": brandingLanguageViewportModes["mobile-fr-igify"],
-  "mobile-ar-dipsea": brandingLanguageViewportModes["mobile-ar-dipsea"],
   "desktop-en-default": brandingLanguageViewportModes["desktop-en-default"],
   "desktop-fr-igify": brandingLanguageViewportModes["desktop-fr-igify"],
-  "desktop-ar-dipsea": brandingLanguageViewportModes["desktop-ar-dipsea"],
 };
 
 export const allModes = {
