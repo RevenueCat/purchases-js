7ddeddc03c69abe4c80558afe1a38c7988b88194
Toni Rico <toni.rico.diez@revenuecat.com>
2025-08-12 09:51:18 +0200

    Add internal API to postSimulatedStoreReceipt (#561)
    ## Motivation / Description
    In this PR we add a new API to allow to post a purchase for a product in
    the simulated store. This will be used by the React native SDK when
    running in expo go mode, where it will need to post the receipt but
    needs to handle the UI differently than how it's handled on browser
    platforms.
    
    Usage of this API requires to explicitely `// @ts-ignore` when calling
    this method.
    
    This API is not meant to be used by others, only meant for Internal
    RevenueCat usage.
    
    ## Changes introduced
    
    ## Linear ticket (if any)
    
    ## Additional comments

---
+++ src/helpers/simulated-store-post-receipt-helper.ts
@@ -0,0 +1,40 @@
+import type { Product } from "../entities/offerings";
+import type { Backend } from "../networking/backend";
+import type { PurchaseResult } from "../entities/purchase-result";
+import { generateUUID } from "./uuid-helper";
+import type { StoreTransaction } from "../entities/store-transaction";
+import { toCustomerInfo } from "../entities/customer-info";
+
+export async function postSimulatedStoreReceipt(
+  product: Product,
+  backend: Backend,
+  appUserId: string,
+): Promise<PurchaseResult> {
+  const purchaseDate = new Date();
+  const fetchToken = `test_${purchaseDate.getTime()}_${generateUUID()}`;
+  const operationSessionId = `test_store_operation_session_${generateUUID()}`;
+  const storeTransactionId = fetchToken;
+
+  const storeTransaction: StoreTransaction = {
+    storeTransactionId,
+    productIdentifier: product.identifier,
+    purchaseDate: purchaseDate,
+  };
+
+  const subscriberResponse = await backend.postReceipt(
+    appUserId,
+    product.identifier,
+    fetchToken,
+    product.presentedOfferingContext,
+    "purchase",
+  );
+
+  const customerInfo = toCustomerInfo(subscriberResponse);
+
+  return {
+    customerInfo,
+    redemptionInfo: null,
+    operationSessionId,
+    storeTransaction,
+  };
+}
