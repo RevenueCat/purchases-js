b64bb5e3ae765ab8ecf376a9bb044e11caa76a7b
Will Taylor <wtaylor151@gmail.com>
2025-08-13 15:16:28 -0500

    remove getCachedVirtualCurrencies and invalidateCachedVirtualCurrencies

--- src/main.ts
+++ src/main.ts
@@ -82,11 +82,8 @@ import type { ReservedCustomerAttribute } from "./entities/attributes";
 import { purchaseSimulatedStoreProduct } from "./helpers/simulated-store-purchase-helper";
 import { postSimulatedStoreReceipt } from "./helpers/simulated-store-post-receipt-helper";
 import { InMemoryCache } from "./helpers/in-memory-cache";
-import type {
-  VirtualCurrencies} from "./entities/virtual-currencies";
-import {
-  toVirtualCurrencies
-} from "./entities/virtual-currencies";
+import type { VirtualCurrencies } from "./entities/virtual-currencies";
+import { toVirtualCurrencies } from "./entities/virtual-currencies";
 
 export { ProductType } from "./entities/offerings";
 export type {
@@ -935,28 +932,6 @@ export class Purchases {
    */
   public async getVirtualCurrencies(): Promise<VirtualCurrencies> {
     return await this._getVirtualCurrenciesForUserId(this._appUserId);
-  }
-
-  /**
-   * The currently cached {@link VirtualCurrencies} if one is available.
-   * This value will remain null until virtual currencies have been fetched at
-   * least once with {@link Purchases.getVirtualCurrencies} or an equivalent function.
-   *
-   * @returns {VirtualCurrencies | null} The currently cached virtual currencies for the current subscriber.
-   */
-  public getCachedVirtualCurrencies(): VirtualCurrencies | null {
-    return this.inMemoryCache.getCachedVirtualCurrencies(this._appUserId);
-  }
-
-  /**
-   * Invalidates the cache for virtual currencies.
-   *
-   * This is useful for cases where a virtual currency's balance might have been updated
-   * outside of the website, like if you decreased a user's balance from the user spending a virtual currency,
-   * or if you increased the balance from your backend using the server APIs.
-   */
-  public invalidateCachedVirtualCurrencies(): void {
-    this.inMemoryCache.invalidateVirtualCurrenciesCache(this._appUserId);
   }
 
   /**
