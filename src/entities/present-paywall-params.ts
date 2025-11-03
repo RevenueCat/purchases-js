db88f4130f4c8410e14be834759dd8c40dd3b291
Nicola Sacco <nicola@revenuecat.com>
2025-11-03 15:50:10 +0100

    WEB-3390: Docs follow-up (#638)
    ## Changes introduced
    * Updated the docs for the PresentPaywallParams interface
    * Exported the interface from the main entrypoint

--- src/entities/present-paywall-params.ts
+++ src/entities/present-paywall-params.ts
@@ -11,17 +11,41 @@ export interface PresentPaywallParams {
    */
   readonly offering: Offering;
 
+  /**
+   * The target element where the paywall will be rendered.
+   * The paywall will create a full-screen overlay if null.
+   */
   readonly htmlTarget?: HTMLElement;
 
+  /**
+   * The target element where the checkout flow will be rendered.
+   * The checkout flow will create a full-screen overlay if null.
+   */
   readonly purchaseHtmlTarget?: HTMLElement;
 
+  /**
+   * The email of the customer starting the purchase.
+   * If passed the checkout flow will not ask for it to the customer.
+   */
   readonly customerEmail?: string;
 
+  /**
+   * Callback to be called when the paywall tries to navigate to an external URL.
+   */
   readonly onNavigateToUrl?: (url: string) => void;
 
+  /**
+   * Callback to be called when the paywall tries to navigate back.
+   */
   readonly onBack?: () => void;
 
+  /**
+   * Callback to be called when the paywall tries to visit the customer center.
+   */
   readonly onVisitCustomerCenter?: () => void;
 
+  /**
+   * The locale to use for the paywall and the checkout flow.
+   */
   readonly selectedLocale?: string;
 }
