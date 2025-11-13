e9b424809b87ec1924ab9c994f0ba5cbecba7f48
vicfergar <victor@revenuecat.com>
2025-11-13 14:11:26 +0100

    refactor: remove `allowPaddleAPIKey` config flag as it is no longer needed

--- src/main.ts
+++ src/main.ts
@@ -324,7 +324,7 @@ export class Purchases {
   }
 
   private static validateConfig(config: PurchasesConfig) {
-    validateApiKey(config.apiKey, config.flags);
+    validateApiKey(config.apiKey);
     validateAppUserId(config.appUserId);
     validateProxyUrl(config.httpConfig?.proxyURL);
     validateAdditionalHeaders(config.httpConfig?.additionalHeaders);
