1b0f85b5216b40e75e2b4d000bccafacacc042bd
Nicola Sacco <nicola@revenuecat.com>
2025-12-17 14:53:54 +0100

    Return the selected package when purchasing from Paywall (#697)
    ## Motivation / Description
    When purchasing through the `presentPaywall` method we do not know which
    package was selected in the paywall when we receive the purchase result.
    
    This PR extends the purchase result to return the selectedPackage.
    
    ## Changes introduced
    Exposed the selected package in the result from the purchase initiated
    from a paywall

--- src/entities/purchase-result.ts
+++ src/entities/purchase-result.ts
@@ -1,6 +1,7 @@
 import type { CustomerInfo } from "./customer-info";
 import type { RedemptionInfo } from "./redemption-info";
 import type { StoreTransaction } from "./store-transaction";
+import type { Package } from "./offerings";
 
 /**
  * Represents the result of a purchase operation.
@@ -25,4 +26,15 @@ export interface PurchaseResult {
    * The store transaction associated with the purchase.
    */
   readonly storeTransaction: StoreTransaction;
+}
+
+/**
+ * Represents the result of a purchase operation initiated from a paywall.
+ * @public
+ */
+export interface PaywallPurchaseResult extends PurchaseResult {
+  /**
+   * The package selected by the customer while interacting with the paywall.
+   */
+  selectedPackage: Package;
 }
