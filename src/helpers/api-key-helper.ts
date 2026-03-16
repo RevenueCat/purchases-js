feb7bcd6f003c810805fe641817e8e173ba53fb8
Nicola Sacco <nicola@revenuecat.com>
2026-03-16 17:32:59 +0100

    Limiting to sandbox api keys for now

--- src/helpers/api-key-helper.ts
+++ src/helpers/api-key-helper.ts
@@ -16,7 +16,11 @@ export function isPaddleApiKey(apiKey: string): boolean {
 }
 
 export function isStripeApiKey(apiKey: string): boolean {
-  return apiKey ? stripe_api_key_regex.test(apiKey) : false;
+  const isStripeApiKey = apiKey ? stripe_api_key_regex.test(apiKey) : false;
+  const isSandboxStripeApiKey = apiKey ? apiKey.startsWith("strp_sb_") : false;
+  // Supporting only sandbox api keys for now, we'll add support for live api keys
+  // when we make the feature generally available
+  return isStripeApiKey && isSandboxStripeApiKey;
 }
 
 export function isSimulatedStoreApiKey(apiKey: string): boolean {
