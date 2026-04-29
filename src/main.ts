ecf91be4ceaa14ede746d13bd486dd0ed377ea53
Jamie Holwill <jholwill@hotmail.com>
2026-04-29 09:56:15 +0100

    Update root paywall element to include required styles for updated purchases-ui-js package

--- src/main.ts
+++ src/main.ts
@@ -534,9 +534,11 @@ export class Purchases {
       element.style.width = "100%";
       element.style.height = "100%";
       element.style.overflow = "auto";
-      element.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+      element.style.backgroundColor = "var(--rc-purchases-ui-bg-color, Canvas)";
+      element.style.colorScheme = "light dark";
       if (doc.body.offsetWidth > 968) {
         element.style.display = "flex";
+        element.style.flexDirection = "column";
         element.style.justifyContent = "center";
         element.style.alignItems = "center";
       }
