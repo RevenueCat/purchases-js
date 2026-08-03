dae7b92190bd31522e7aa3ee4e514f965814eff3
Will Taylor <wtaylor151@gmail.com>
2026-08-03 10:55:05 -0500

    remove unneeded ignoreAllLogs

--- examples/amazonvega-demo/PurchaseTester/index.js
+++ examples/amazonvega-demo/PurchaseTester/index.js
@@ -1,9 +1,5 @@
-import {AppRegistry, LogBox} from 'react-native';
+import {AppRegistry} from 'react-native';
 import {App} from './src/App';
 import {name as appName} from './app.json';
-
-// Temporary workaround for problem with nested text
-// not working currently.
-LogBox.ignoreAllLogs();
 
 AppRegistry.registerComponent(appName, () => App);
