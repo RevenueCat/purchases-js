4c42605fff1de8d4f4690ef42b21dbc03714877b
Will Taylor <wtaylor151@gmail.com>
2026-08-03 10:57:10 -0500

    cleanup

--- examples/amazonvega-demo/PurchaseTester/metro.config.js
+++ examples/amazonvega-demo/PurchaseTester/metro.config.js
@@ -2,11 +2,11 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const path = require('path');
 
 /**
-+ * Metro configuration
-+ * https://facebook.github.io/metro/docs/configuration
-  *
-+ * @type {import('metro-config').MetroConfig}
-  */
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const config = {
   watchFolders: [path.resolve(__dirname, '../../..')],
   resolver: {
