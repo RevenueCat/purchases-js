bd81721e4f6a5c71af9fdbca33c6c8ed64cee26a
Will Taylor <wtaylor151@gmail.com>
2026-08-04 14:59:19 -0500

    all offerings list

--- examples/amazonvega-demo/PurchaseTester/src/screens/HomeScreen.tsx
+++ examples/amazonvega-demo/PurchaseTester/src/screens/HomeScreen.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {AllOfferingsList} from '../components/AllOfferingsList';
 import {ScreenContainer} from '../ScreenContainer';
-import {styles} from './HomeScreen.styles';
 
 export const HomeScreen = () => (
   <ScreenContainer subtitle="Home">
-    <View style={styles.content}>
-      <View style={styles.statusCard}>
-        <Text style={styles.statusTitle}>RevenueCat SDK configured</Text>
-        <Text style={styles.statusDescription}>
-          The Purchase Tester app is ready to go!
-        </Text>
-      </View>
-    </View>
+    <AllOfferingsList />
   </ScreenContainer>
 );
