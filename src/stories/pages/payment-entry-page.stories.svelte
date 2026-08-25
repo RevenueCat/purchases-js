fac21b5dcdeb45993b8a45bef32ab46e161c5b4d
Nicola Sacco <nicola@revenuecat.com>
2026-08-25 18:00:57 +0200

    test(purchases-js): allow slower Stripe readiness

--- src/stories/pages/payment-entry-page.stories.svelte
+++ src/stories/pages/payment-entry-page.stories.svelte
@@ -56,7 +56,7 @@
         async () => {
           await expect(canvas.getByTestId("PayButton")).toBeVisible();
         },
-        { timeout: 10_000 },
+        { timeout: 30_000 },
       );
     },
     // @ts-ignore ignore importing before initializing
